259f336bb99a246c37c96761e7475a84a5f3fc2a
Seingshin Lee <seingshinlee@gmail.com>
2019-12-03 18:23:27 +0800

    Update "build.js": generate dists:
      - ./dists/dark/sunrise-dark.json
      - ./dists/dark/sunrise-dark-bold.json
      - ./dists/dark/sunrise-dark-italic.json

--- assets/yaml/dark/build.js
+++ assets/yaml/dark/build.js
@@ -2,10 +2,12 @@
 const fs = require("fs");
 const js_yaml = require("js-yaml");
 const path = require("path");
+const mustache = require("mustache");
 
 const colors = js_yaml.safeLoad(fs.readFileSync("./assets/yaml/dark/colors.yaml"));
 const colors_template = JSON.stringify(js_yaml.safeLoad(fs.readFileSync("./assets/yaml/dark/colors.yaml")));
 const template = JSON.stringify(js_yaml.safeLoad(fs.readFileSync("./assets/yaml/dark/template.yaml")));
+const ui_and_synatx = JSON.parse(mustache.render(colors_template, { ...colors }));
 
 function mkdirsSync(dirname) {
     if (fs.existsSync(dirname)) {
@@ -19,3 +21,11 @@ function mkdirsSync(dirname) {
 }
 
 mkdirsSync("./assets/json/dark");
+mkdirsSync("./dists/dark");
+
+const themeNames = ["sunrise-dark", "sunrise-dark-bold", "sunrise-dark-italic"];
+for (let themeName of themeNames) {
+    const header = js_yaml.safeLoad(fs.readFileSync(`./assets/yaml/dark/${themeName}.yaml`));
+    const theme = mustache.render(template, { ...header, ...ui_and_synatx });
+    fs.writeFileSync(`./dists/dark/${themeName}.json`, theme);
+}
