7da84db4963af7109741a66243872914996d44a0
Seingshin Lee <seingshinlee@gmail.com>
2019-12-03 18:17:27 +0800

    Update "build.js": add a function "mkdirsSync()"

--- assets/yaml/dark/build.js
+++ assets/yaml/dark/build.js
@@ -1,7 +1,21 @@
 'use strict'
 const fs = require("fs");
 const js_yaml = require("js-yaml");
+const path = require("path");
 
 const colors = js_yaml.safeLoad(fs.readFileSync("./assets/yaml/dark/colors.yaml"));
 const colors_template = JSON.stringify(js_yaml.safeLoad(fs.readFileSync("./assets/yaml/dark/colors.yaml")));
 const template = JSON.stringify(js_yaml.safeLoad(fs.readFileSync("./assets/yaml/dark/template.yaml")));
+
+function mkdirsSync(dirname) {
+    if (fs.existsSync(dirname)) {
+        return true;
+    } else {
+        if (mkdirsSync(path.dirname(dirname))) {
+            fs.mkdirSync(dirname);
+            return true;
+        }
+    }
+}
+
+mkdirsSync("./assets/json/dark");
